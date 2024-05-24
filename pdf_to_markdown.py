5a83873fe0b23bc2b93e23f9dae074f2d1899c98
Abbey Julian <abbey.roberts@att.net>
2024-05-24 14:48:47 -0400

    version 2.01: confirmation popup, error handling and logging, double letters/numbers, ambiguous headings

--- pdf_to_markdown.py
+++ pdf_to_markdown.py
@@ -2,6 +2,7 @@ import pdfplumber
 import re
 import tkinter as tk
 from tkinter import filedialog
+import logging
 
 print("PDF Outline to Markdown Conversion")
 print("updated May 2024 by abbeyjulian")
@@ -11,6 +12,11 @@ print("---------------------------------")
 class Filenames:
     def __init__(self):
         self.filenames = []
+
+
+# for error reporting
+logging.basicConfig(filename='pdf_to_markdown.log', level=logging.ERROR, format='%(asctime)s %(levelname)s %(name)s %(message)s')
+logger = logging.getLogger(__name__)
 
 
 window = tk.Tk()
@@ -66,97 +72,125 @@ headings_label.pack()
 headings_entry.pack()
 
 def handle_click(event):
+    success = []
+    fail = []
     for i in range(len(filenames.filenames)):
-        print("Converting...{}/{}".format(i, len(filenames.filenames)))
-        filepath = filenames.filenames[i]
-        filetype = filepath.split(".")[-1]
-        print("file type is:", filetype)
-        filename = "".join(filepath.split(".")[:-1])
-        print("file name is:", filename)
-        pdf = pdfplumber.open(filepath)
-        header_length = int(header_entry.get())
-        footer_length = int(footer_entry.get())
-        styles = styles_entry.get().split(",")
-        styles = [s.strip() for s in styles]
-        number_levels = len(styles)
-        # set for REGEX matching
-        styles = [s.replace("X", r'\d') for s in styles]
-        styles = [s.replace("Y", r'[A-Z]') for s in styles]
-        styles = [s.replace("y", r'[a-z]') for s in styles]
-        styles = [s.replace("R", r'[A-Z]+') for s in styles]
-        styles = [s.replace("r", r'[a-z]+') for s in styles]
-        styles = [s.replace(".", r'\.') for s in styles]
-        styles = [s.replace(")", r'\)') for s in styles]
-        styles = [s.replace("(", r'\(') for s in styles]
-        number_headings = int(headings_entry.get())
-        current_level = 0
-        md_text = ""
-        for page in pdf.pages:
-            # get text and remove header/footer from page text
-            text = page.extract_text()
-            text = text.split("\n")[header_length:-footer_length]
-            for t in text:
-                matched = False
-                # same level
-                if re.match(styles[current_level], t):
-                    md_text += "\n"
-                    if current_level < number_headings:
-                        md_text += ("#"*(current_level+1))
+        try:
+            print("Converting...{}/{}".format(i, len(filenames.filenames)))
+            filepath = filenames.filenames[i]
+            filetype = filepath.split(".")[-1]
+            print("file type is:", filetype)
+            filename = "".join(filepath.split(".")[:-1])
+            print("file name is:", filename)
+            pdf = pdfplumber.open(filepath)
+            header_length = int(header_entry.get())
+            footer_length = int(footer_entry.get())
+            styles = styles_entry.get().split(",")
+            styles = [s.strip() for s in styles]
+            number_levels = len(styles)
+            # set for REGEX matching
+            styles = [s.replace("X", r'\d+') for s in styles]
+            styles = [s.replace("Y", r'[A-Z]+') for s in styles]
+            styles = [s.replace("y", r'[a-z]+') for s in styles]
+            styles = [s.replace("R", r'[A-Z]+') for s in styles]
+            styles = [s.replace("r", r'[a-z]+') for s in styles]
+            styles = [s.replace(".", r'\.') for s in styles]
+            styles = [s.replace(")", r'\)') for s in styles]
+            styles = [s.replace("(", r'\(') for s in styles]
+            styles = [s.strip() for s in styles]
+            styles = [s+" " for s in styles]
+            number_headings = int(headings_entry.get())
+            current_level = 0
+            md_text = ""
+            for page in pdf.pages:
+                # get text and remove header/footer from page text
+                text = page.extract_text()
+                text = text.split("\n")[header_length:-footer_length]
+                for t in text:
+                    matched = False
+                    # same level
+                    if re.match(styles[current_level], t):
+                        md_text += "\n"
+                        if current_level < number_headings:
+                            md_text += ("#"*(current_level+1))
+                            md_text += " "
+                        else:
+                            md_text += ("\n"+"\t"*(current_level-number_headings))
+                        md_text += t
+                        matched = True
+                    # up level(s)
+                    elif current_level-1 >= 0:
+                        cl = current_level
+                        while cl-1 >= 0:
+                            if re.match(styles[cl-1], t):
+                                md_text += "\n"
+                                current_level = cl-1
+                                if current_level < number_headings:
+                                    md_text += ("#" * (current_level + 1))
+                                    md_text += " "
+                                else:
+                                    md_text += ("\n" + "\t" * (current_level - number_headings))
+                                md_text += t
+                                matched = True
+                                break
+                            cl -= 1
+                    # down a level
+                    if not matched and current_level+1 < number_levels and re.match(styles[current_level+1], t):
+                        md_text += "\n"
+                        current_level += 1
+                        if current_level < number_headings:
+                            md_text += ("#" * (current_level + 1))
+                            md_text += " "
+                        else:
+                            md_text += ("\n"+"\t"*(current_level-number_headings))
+                        md_text += t
+                        matched = True
+                    # continuation of paragraph
+                    if not matched:
                         md_text += " "
-                    else:
-                        md_text += ("\n"+"\t"*(current_level-number_headings))
-                    md_text += t
-                    matched = True
-                # up level(s)
-                elif current_level-1 >= 0:
-                    cl = current_level
-                    while cl-1 >= 0:
-                        if re.match(styles[cl-1], t):
-                            md_text += "\n"
-                            current_level = cl-1
-                            if current_level < number_headings:
-                                md_text += ("#" * (current_level + 1))
-                                md_text += " "
-                            else:
-                                md_text += ("\n" + "\t" * (current_level - number_headings))
-                            md_text += t
-                            matched = True
-                            break
-                        cl -= 1
-                # down a level
-                if not matched and current_level+1 < number_levels and re.match(styles[current_level+1], t):
-                    md_text += "\n"
-                    current_level += 1
-                    if current_level < number_headings:
-                        md_text += ("#" * (current_level + 1))
-                        md_text += " "
-                    else:
-                        md_text += ("\n"+"\t"*(current_level-number_headings))
-                    md_text += t
-                    matched = True
-                # continuation of paragraph
-                if not matched:
-                    md_text += " "
-                    md_text += t
-            # handle table(s)
-            table = page.extract_tables(table_settings={})
-            for _table in table:
-                unformatted_table = ""
-                formatted_table = ""
-                header = True
-                for row in _table:
-                    unformatted_table += (" ".join(row) + " ")
-                    formatted_table += ("| " + " | ".join(row) + " |\n")
-                    if header:
-                        formatted_table += ("| " + ("---|")*len(row)+"\n")
-                        header = False
-                unformatted_table = unformatted_table[:-1]  # remove space at end
-                formatted_table = "\n\n" + formatted_table  # preprend newlines
-                md_text = md_text.replace(unformatted_table, formatted_table)
-        # save to markdown file
-        with open(filename + ".md", 'w') as file:
-            file.write(md_text)
-        print("Saved to ", filename, ".md")
+                        md_text += t
+                # handle table(s)
+                table = page.extract_tables(table_settings={})
+                for _table in table:
+                    unformatted_table = ""
+                    formatted_table = ""
+                    header = True
+                    for row in _table:
+                        row = ["" if r is None else r for r in row]
+                        unformatted_table += (" ".join(row) + " ")
+                        formatted_table += ("| " + " | ".join(row) + " |\n")
+                        if header:
+                            formatted_table += ("| " + ("---|")*len(row)+"\n")
+                            header = False
+                    unformatted_table = unformatted_table[:-1]  # remove space at end
+                    formatted_table = "\n\n" + formatted_table  # preprend newlines
+                    md_text = md_text.replace(unformatted_table, formatted_table)
+            # save to markdown file
+            with open(filename + ".md", 'w') as file:
+                file.write(md_text)
+            print("Saved to ", filename, ".md")
+            success.append(i)
+        except Exception as e:
+            logger.log(logging.ERROR, "error with file {}: ".format(filenames.filenames[i]))
+            logger.exception(e)
+            fail.append(i)
+            continue
+
+    popup = tk.Toplevel()
+    confirmation = tk.Label(popup, text="Conversion complete.")
+    confirmation.pack()
+    successful_count = tk.Label(popup, text="{}/{} files successfully converted.".format(len(success), len(filenames.filenames)))
+    successful_count.pack()
+    if len(success) < len(filenames.filenames):
+        errorlist = "The following files encountered a conversion error: \n"
+        for i in fail:
+            errorlist += (filenames.filenames[i] + "\n")
+        errorlist += "See pdf_to_markdown.log for error details."
+        errorlist_label = tk.Label(popup, text=errorlist)
+        errorlist_label.pack()
+    ok_button = tk.Button(popup, text="OK", command=popup.destroy)
+    ok_button.pack()
+    popup.mainloop()
 
 
 framea = tk.Frame(relief=tk.RAISED)
